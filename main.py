ed7e4e0ca433381ca59ff7f3a91cd77ea42e79db
Ozan Ozbeker <github@ozanozbeker.com>
2025-03-24 18:36:57 -0400

    add splitting to asteroids

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
 
             for shot in shots:
                 if asteroid.is_colliding(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
