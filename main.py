151038ef5176ef66dabdbff3d86b640540c6800d
Ozan Ozbeker <github@ozanozbeker.com>
2025-03-24 17:16:39 -0400

    added player

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+import constants as cons
+from player import Player
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    game_clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x=cons.SCREEN_WIDTH / 2, y=cons.SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -18,9 +19,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = game_clock.tick(60) / 1000
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
