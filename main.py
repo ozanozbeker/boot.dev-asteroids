1ab115de121d90266de89b1d94257d58162c9fd3
Ozan Ozbeker <github@ozanozbeker.com>
2025-03-24 17:56:45 -0400

    added collision for player vs asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 import constants as cons
 
@@ -13,30 +14,35 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = updatable
-
-    player = Player(x=cons.SCREEN_WIDTH / 2, y=cons.SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x=cons.SCREEN_WIDTH / 2, y=cons.SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.is_colliding(player):
+                sys.exit("Game over!")
+
         screen.fill("black")
 
         for item in drawable:
             item.draw(screen)
-
-        updatable.update(dt)
 
         pygame.display.flip()
 
